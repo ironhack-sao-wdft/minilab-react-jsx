8e72b92b3d6d859b63294562cb31452482ca06e6
andre.irece <andre.irece@gmail.com>
2022-03-05 10:46:34 -0300

    done

---
+++ src/components/EmotionMeter.js
@@ -0,0 +1,16 @@
+let emotion = "HAPPY";
+
+
+
+function Humor() {
+//   return emotion === "HAPPY" ? (
+//     <i className="far fa-grin-alt fa-5x"></i>
+//   ) : (
+//     <i className="far fa-frown fa-5x"></i>
+//   );
+if (emotion === "HAPPY"){
+    return <i className="far fa-grin-alt fa-5x"></i>
+} return <i className="far fa-frown fa-5x"></i>
+}
+
+export default Humor;
